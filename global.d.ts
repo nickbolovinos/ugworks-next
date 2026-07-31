ecdbd7f6d735246a61aeadc5d81356e1a6bd06ef
Nick Bolovinos <greekdish@aol.com>
2026-07-31 15:38:32 -0400

    Updated htaccess file to bypass server verification all the time. Resolved Build error for "Type error: Cannot find module or type declarations for side-effect import of '@/styles/styles.css'."

---
+++ global.d.ts
@@ -0,0 +1,9 @@
+// Type declarations for importing CSS/SCSS files in TypeScript
+// This allows side-effect imports like `import '@/styles/styles.css'` in Next.js
+
+declare module '*.css';
+declare module '*.scss';
+declare module '*.sass';
+declare module '*.module.css';
+declare module '*.module.scss';
+declare module '*.module.sass';
